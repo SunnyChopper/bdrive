d51a750e7ce5888f35d4609f670c635275587486
Sunny Singh <ishy.singh@gmail.com>
2018-12-27 01:49:42 -0600

    Added favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,8 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+		<link rel="icon" href="{{ URL::asset('images/favicon.png') }}">
+
 		@if(isset($page_title))
 			<title>{{ $page_title }} - {{ env('APP_NAME') }}</title>
 		@else
